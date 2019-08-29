576bab7fbbc45e4e223537d0558c1bd84c57f19a
aki <lala.akira@gmail.com>
2019-08-30 05:18:04 +0900

    Improve snippet

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to the "rails-routes" extension will be documented in this file.
 
+## 0.3.1
+
+- Improve snippet
+
 ## 0.3.0
 
 - Add `railsRoutes.insert` command
--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,7 @@
 import { SnippetString } from "vscode";
 
 export function buildSnippet(helper: string, params: string[]) {
-  let snippet = `${helper}_\${1:path}`;
+  let snippet = `${helper}_\${1|path,url|}`;
 
   if (params.length > 0) {
     const args = params.map((param, index) => `\${${index + 2}:${param}}`);
